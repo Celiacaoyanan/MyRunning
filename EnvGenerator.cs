4a0b4ad85bf0fc2204a3966a453e22f1e1d480c7
Celiacaoyanan <celiacaoyanan@outlook.com>
2016-10-16 16:11:36 +0800

    Change to English annotation

--- EnvGenerator.cs
+++ EnvGenerator.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Generate the environment
+// There are 3 types of forests, we generate environment by the joint of the three forests.
+// There will only be 2 forests at the same time,one is which the player is in now, the other is which the player will be in later.
+// When the player runs out of a forest, this forest will be removed, and use random number to generate the next forest
+
 public class EnvGenerator : MonoBehaviour {
 
 	public Forest forest1;
